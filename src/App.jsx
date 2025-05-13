751cdc95a97aed4160d2c0383d5b3bc3b1150b80
sanjibRanjan <iam007sanju@gmail.com>
2025-05-13 11:41:16 +0530

    Fix: Add missing wagmi dependency

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,7 @@ export default function App() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-4 justify-center text-center">
-      {signerStatus.isInitializing ? (
-        <>Loading...</>
-      ) : user ? (
+      { user ? (
         <div className="flex flex-col gap-2 p-2">
           <p className="text-xl font-bold">Success!</p>
           You're logged in as {user.email ?? "anon"}.<button
